a22d60d573280b92db150f9d919388faf5f62e7c
Sophya <sophyatamalintseva@yandex.ru>
2024-12-20 20:04:47 +0700

    Изменение внешнего вида проигрывателя, добавление некоторых кнопок

--- MyForm1.h
+++ MyForm1.h
@@ -36,6 +36,11 @@ namespace Kurs2Work {
 		}
 	private: AxWMPLib::AxWindowsMediaPlayer^ axWindowsMediaPlayer1;
 	private: AxWMPLib::AxWindowsMediaPlayer^ axWindowsMediaPlayer2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::TrackBar^ trackBar1;
 	protected:
 
 	private:
@@ -53,27 +58,83 @@ namespace Kurs2Work {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm1::typeid));
 			this->axWindowsMediaPlayer2 = (gcnew AxWMPLib::AxWindowsMediaPlayer());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// axWindowsMediaPlayer2
 			// 
 			this->axWindowsMediaPlayer2->Enabled = true;
-			this->axWindowsMediaPlayer2->Location = System::Drawing::Point(171, 44);
+			this->axWindowsMediaPlayer2->Location = System::Drawing::Point(2, 0);
 			this->axWindowsMediaPlayer2->Name = L"axWindowsMediaPlayer2";
 			this->axWindowsMediaPlayer2->OcxState = (cli::safe_cast<System::Windows::Forms::AxHost::State^>(resources->GetObject(L"axWindowsMediaPlayer2.OcxState")));
-			this->axWindowsMediaPlayer2->Size = System::Drawing::Size(187, 143);
+			this->axWindowsMediaPlayer2->Size = System::Drawing::Size(1043, 635);
 			this->axWindowsMediaPlayer2->TabIndex = 0;
 			this->axWindowsMediaPlayer2->Enter += gcnew System::EventHandler(this, &MyForm1::axWindowsMediaPlayer2_Enter_1);
 			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(1080, 41);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(1080, 87);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(1080, 595);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(1080, 137);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"button4";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// trackBar1
+			// 
+			this->trackBar1->Location = System::Drawing::Point(1064, 184);
+			this->trackBar1->Name = L"trackBar1";
+			this->trackBar1->Size = System::Drawing::Size(104, 56);
+			this->trackBar1->TabIndex = 5;
+			// 
 			// MyForm1
 			// 
-			this->ClientSize = System::Drawing::Size(632, 370);
+			this->ClientSize = System::Drawing::Size(1190, 639);
+			this->Controls->Add(this->trackBar1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->axWindowsMediaPlayer2);
 			this->Name = L"MyForm1";
 			this->Load += gcnew System::EventHandler(this, &MyForm1::MyForm1_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
